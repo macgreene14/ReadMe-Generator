63a8a02355f092e2b258dfacc4d1bbce8fe552fc
jmg <macgreene14@gmail.com>
2022-09-07 11:23:14 -0400

    improved functionality of application

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const inquirer = require("inquirer")
 const fs = require("fs")
-const generateMarkdown = require("./utils/generateMarkdown")
+const generateMarkdown = require("./utils/generateMarkdown")  // import local function 
 
 
 const questions = [
-    {name: 'title', message: 'Project Title:' },
-    {name: 'description', message: 'Project Description:'},
-    {name: 'installation', message: 'Project Installation:'}, 
-    {name: 'usage', message: 'Project Usage:'}, 
-    {name: 'license', message: 'Project License:'}, 
-    {name: 'contributing', message: 'Project Contributions:'}, 
-    {name: 'tests', message: 'Tests:'}, 
-    {name: 'email', message: 'Author Email:'}, 
-    {name: 'github', message: 'Author Gihub Profile:'},
+    {type: 'input', name: 'title', message: 'Project Title:' },
+    {type: 'input', name: 'description', message: 'Provide a short description explaining the what, why, and how of your project:'},
+    {type: 'input', name: 'installation', message: 'Provide the steps required to install your project:'}, 
+    {type: 'input', name: 'usage', message: 'Provide instructions and examples for use:'}, 
+    {type: 'list', name: 'license', message: 'Choose a License:', choices: ['Apache License 2.0', 'MIT License','GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Boost Software License 1.0', 'The Unlicense']},
+    {type: 'input', name: 'contributing', message: 'Provide guidelines for contributing to this project:'}, 
+    {type: 'input', name: 'tests', message: 'Provide instructions on how to run tests:'}, 
+    {type: 'input', name: 'github', message: 'Author Gihub Profile:'},
+    {type: 'input', name: 'email', message: 'Author Email:'}, 
 ];
 
 function writeToFile(fileName, data) {
